68f6845c28ddfe9ce0be93f34f7607ea67669a21
sarath-crypto <76784482+sarath-crypto@users.noreply.github.com>
2024-03-21 11:14:51 +0530

    Update srec.sh

--- srec.sh
+++ srec.sh
@@ -1,2 +1,2 @@
 srec_cat bootloader.hex -intel -exclude 0x0000 0x3800 -o bootloader.hex -intel
-srec_cat app.hex -intel bootloader.hex -intel -offset -minimum-addr security.hex -intel -o firmw.hex -intel
+srec_cat app.hex -intel bootloader.hex -intel -offset -minimum-addr app.hex -intel -o firmw.hex -intel
